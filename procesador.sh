46b1515172e915ecf65fb24792ae47dcbbd296af
Odrallag100 <jrgallardo_1@hotmail.com>
2026-08-23 21:54:23 -0600

    feat: agregar script procesador.sh para filtrar ERROR y WARNING

--- procesador.sh
+++ procesador.sh
@@ -5,3 +5,9 @@ mkdir -p resultados
 
 # Extraer líneas con "ERROR" de sistema.log y guardarlas en resultados/errores.log
 grep "ERROR" sistema.log > resultados/errores.log
+
+# Extraer líneas con "WARNING" de sistema.log
+grep "WARNING" sistema.log > resultados/alertas.log
+
+# Eliminar archivos temporales .tmp en el directorio actual
+rm -f *.tmp
